c246356d30019f2c9cda66ffe0939a68cac832be
ryn <raynrayan1341@gmail.com>
2024-11-24 08:42:20 +0700

    selesai fitur-example

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('home', function () {
+    return view('home');
+});
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
